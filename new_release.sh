f72ddd75bd0d006a343e6be0b701cdb3ab39b1c2
Marios Karagiannopoulos <mariosk@gmail.com>
2024-03-06 20:16:35 +0000

    fix(bump2version is not needed in raspirri_app)

--- new_release.sh
+++ new_release.sh
@@ -15,8 +15,6 @@ if [ "$PART" != "major" ] && [ "$PART" != "minor" ] && [ "$PART" != "patch" ]; t
     usage
 fi
 
-pip install bump2version
-
 # Get the latest tag
 LATEST_TAG=$(git describe --tags --abbrev=0)
 
@@ -46,10 +44,7 @@ case $PART in
         ;;
 esac
 
-bump2version --new-version ${NEW_VERSION} ${PART} --config-file bumpversion.cfg
-if [ $? -ne 0 ]; then
-    echo "Error occured. Please fix the error and retry!"
-    exit 2
-fi
-git add bumpversion.cfg && git commit -m "Bumping version to v${NEW_VERSION}" && git push
+
+sed -i "s/^version: .*/version: $NEW_VERSION/" pubspec.yaml
+git add pubspec.yaml && git commit -m "Bumping version to v${NEW_VERSION}" && git push
 git tag "v${NEW_VERSION}" && git push origin "v${NEW_VERSION}"
